00f32f0351862749d34a018e636de024ba5b3c8f
F.Cesni.Maldonado <sigeusb@hotmail.com>
2016-03-15 12:58:00 -0600

    primera adicion
    se hizo una copia de produccion del dia 15/03/2016 para empezar a
    codificar en desarrollo

---
+++ scripts/controllers/calcomm.js
@@ -0,0 +1,29 @@
+calcomm.controller('CalcommCtrl',function($scope,Session,CalcommLogin,$location,CalcommUtils){
+	
+	$scope.GoTo = function(url){
+		$location.path(url);
+	};
+	$scope.logout = function(){CalcommUtils.logout();}
+	$scope.showImg = function(url){return CalcommUtils.getProfileImage(url);}
+	$scope.MyStatus = function(){return CalcommUtils.getMyStatus();}
+		
+		
+	if(CalcommLogin.isAuthenticated())
+	{
+		$scope.user = Session.getSession();
+//		console.debug($scope.user);
+		if(CalcommLogin.isAuthenticated()&&!$location.absUrl().indexOf("home")>-1&&!CalcommLogin.isAdmin())
+		{
+			if(!($scope.user.forms.basic && $scope.user.forms.profile && $scope.user.forms.experience && $scope.user.forms.availability && $scope.user.forms.legal) )
+			{	
+				notificar("you must complete all forms.");$location.path("/signup");
+			}
+		}
+		
+
+		if(CalcommLogin.isAdmin())
+			$scope.advance=true;
+		
+	}
+		
+});
